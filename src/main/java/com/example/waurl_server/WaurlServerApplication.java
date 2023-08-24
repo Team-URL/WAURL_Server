d0fb5ebe0ad9945c948306ebfa8c6f625a375da6
박준영 <wnsdud6969@naver.com>
2023-08-24 15:05:07 +0900

    Initial Project

---
+++ src/main/java/com/example/waurl_server/WaurlServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.waurl_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaurlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaurlServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/waurl_server/WaurlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waurl_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaurlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
